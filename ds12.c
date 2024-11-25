d012c526a769ac2db0a29ccee032ab551f1c7665
Bhagesh4604 <bhageshbiradar820@gmail.com>
2024-11-25 13:01:49 +0530

    Add files via upload

--- ds12.c
+++ ds12.c
@@ -1,22 +1,25 @@
 #include <stdio.h> 
 #include <stdlib.h> 
 #include <string.h> 
-#define MAX_NUM_EMPLOYEES 100 
-#define MAX_HASH_TABLE_SIZE 50 
+#define MAX_NUM_EMPLOYEES 100 // Maximum number of employees 
+#define MAX_HASH_TABLE_SIZE 50 // Maximum size of the hash table 
+// Define the structure for an employee record 
 typedef struct 
 { 
- int iKey; 
+ int iKey; // 4-digit key 
  char cName[50]; 
 }EMPLOYEE; 
+// Define the hash table as an array of employee pointers 
 EMPLOYEE* stHashTable[MAX_HASH_TABLE_SIZE]; 
 int fnCompHash(int, int); 
 void fnInsRecord(EMPLOYEE*, int); 
 EMPLOYEE* fnSrchRecord(int, int); 
 int main() 
 { 
- int m; 
+ int m; // Size of the hash table 
  printf("Enter the size of the hash table (m): "); 
- scanf("%d",&m);  
+ scanf("%d",&m); 
+ // Initialize the hash table with NULL pointers 
  for (int i = 0; i < m; i++) 
  { 
  stHashTable[i] = NULL; 
@@ -56,6 +59,7 @@ int main()
 void fnInsRecord(EMPLOYEE* emp, int m) 
 { 
  int index = fnCompHash(emp->iKey, m); 
+ // Linear probing if collisions happen 
  while(stHashTable[index] != NULL) 
  { 
  index = (index + 1) % m; 
@@ -69,6 +73,7 @@ int fnCompHash(int iKey, int m)
 EMPLOYEE* fnSrchRecord(int iKey, int m) 
 { 
  int index = fnCompHash(iKey, m); 
+ // Linear probing 
  while(stHashTable[index] != NULL) 
  { 
  if(stHashTable[index]->iKey == iKey) 
@@ -77,5 +82,5 @@ EMPLOYEE* fnSrchRecord(int iKey, int m)
  } 
  index = (index + 1) % m; 
  } 
- return NULL; 
+ return NULL; // Employee record not found 
 } 
